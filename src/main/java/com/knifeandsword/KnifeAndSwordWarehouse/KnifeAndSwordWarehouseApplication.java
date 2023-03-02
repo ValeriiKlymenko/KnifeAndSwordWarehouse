791718fe47157d80bcff8f6ef4823655860b138a
KlimJavaDev <klimjavadev@gmail.com>
2023-03-02 20:54:18 +0200

    Start project

---
+++ src/main/java/com/knifeandsword/KnifeAndSwordWarehouse/KnifeAndSwordWarehouseApplication.java
@@ -0,0 +1,13 @@
+package com.knifeandsword.KnifeAndSwordWarehouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnifeAndSwordWarehouseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KnifeAndSwordWarehouseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/knifeandsword/KnifeAndSwordWarehouse/KnifeAndSwordWarehouseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.knifeandsword.KnifeAndSwordWarehouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KnifeAndSwordWarehouseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
