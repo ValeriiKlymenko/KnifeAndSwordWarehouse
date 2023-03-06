d65ad6dec0e02c2ae6509a6fe7e9919b60c6dd11
KlimJavaDev <klimjavadev@gmail.com>
2023-03-06 17:39:41 +0200

    fifed edit and add remove iron

--- src/main/java/com/knifeandsword/KnifeAndSwordWarehouse/controllers/IronController.java
+++ src/main/java/com/knifeandsword/KnifeAndSwordWarehouse/controllers/IronController.java
@@ -2,13 +2,11 @@ package com.knifeandsword.KnifeAndSwordWarehouse.controllers;
 
 import com.knifeandsword.KnifeAndSwordWarehouse.entities.Iron;
 import com.knifeandsword.KnifeAndSwordWarehouse.repositories.IronRepository;
-import com.knifeandsword.KnifeAndSwordWarehouse.services.IronService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.math.BigDecimal;
-import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Optional;
 
@@ -20,7 +18,7 @@ public class IronController {
         this.ironRepository = ironRepository;
     }
 
-    @GetMapping("/iron")
+    @GetMapping("/")
     public String ironList(Model model){
         Iterable<Iron> iron = ironRepository.findAll();
         model.addAttribute("iron", iron);
@@ -49,12 +47,52 @@ public class IronController {
     public String ironDetails(@PathVariable(value = "id") long id, Model model){
 
         if (!ironRepository.existsById(id)){ // якщо немає такого id
-            return "redirect:/home";
+            return "redirect:/iron";
         }
         Optional<Iron> iron = ironRepository.findById(id);
         ArrayList<Iron> res = new ArrayList<>();
         iron.ifPresent(res::add);
         model.addAttribute("iron", res);
         return "iron-details";
+    }
+    @GetMapping("/iron/{id}/edit")
+    public String ironEdit(@PathVariable(value = "id") long id, Model model){
+
+        if (!ironRepository.existsById(id)){ // якщо немає такого id
+            return "redirect:/iron/";
+        }
+        Optional<Iron> iron = ironRepository.findById(id);
+        ArrayList<Iron> res = new ArrayList<>();
+        iron.ifPresent(res::add);
+        model.addAttribute("iron", res);
+        return "iron-edit";
+    }
+    @PostMapping("/iron/{id}/edit")
+    public String ironPostUpdate(@PathVariable(value = "id") long id,
+                                 @RequestParam String name,
+                                 @RequestParam int width,
+                                 @RequestParam int height,
+                                 @RequestParam int length,
+                                 @RequestParam int weight,
+                                 @RequestParam int amount,
+                                 @RequestParam BigDecimal price,
+                                 Model model){
+        Iron iron = ironRepository.findById(id).orElseThrow(); // виключення, якщо запис бул не найдений
+        iron.setName(name);
+        iron.setWidth(width);
+        iron.setHeight(height);
+        iron.setLength(length);
+        iron.setWeight(weight);
+        iron.setAmount(amount);
+        iron.setPrice(price);
+        ironRepository.save(iron);
+        return "redirect:/iron/";
+    }
+    @PostMapping("/iron/{id}/remove")
+    public String ironPostDelete(@PathVariable(value = "id") long id, Model model){
+        Iron iron = ironRepository.findById(id).orElseThrow();
+        ironRepository.delete(iron);
+
+        return "redirect:/iron/";
     }
 }
